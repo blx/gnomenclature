3003a43531b224accac5d0476f2516bdba5821ac
Benjamin Cook <bc@benlxc.ca>
2012-10-19 00:41:27 -0400

    Added test implementation of mixed formula-to-name and name-to-formula questions.

--- routes/q.js
+++ routes/q.js
@@ -11,6 +11,9 @@ exports.q = function(req, res) {
     var num = (req.query.n &&
                req.query.n <= _cations.length &&
                req.query.n <= _anions.length) ? parseInt(req.query.n) : 1,
+        qmode = (req.query.mode &&
+                 __.contains(['ftn', 'ntf', 'mixed'], req.query.mode.toLowerCase())) ?
+                 req.query.mode.toLowerCase() : 'mixed',
         obj = [],
         pick = function(z) {return __.chain(z).shuffle().first(num).value();},
         parens = function(radc, n) {return ((radc[3] && n) ? '(' + radc[0] + ')' : radc[0]) + n;};
@@ -23,13 +26,25 @@ exports.q = function(req, res) {
         else if (a === 1) a = '';
         else if (c === 1) c = '';
         
-        obj.push({
-            'question': parens(cat, c) + parens(an, a),
-            
-            'answer': __.map((__.isArray(an[1]) ? an[1] : [an[1]]), function(an) {
+        var q_formula = parens(cat, c) + parens(an, a);
+        var q_name = __.map((__.isArray(an[1]) ? an[1] : [an[1]]), function(an) {
                 return cat[1] + ' ' + an;
-            })
-        });
+            });
+
+        var current_mode = (qmode === 'mixed' ? ['ftn', 'ntf'][__.random(1)] : qmode);
+        
+        if (current_mode === 'ftn') {
+            obj.push({
+                'question': q_formula,
+                'answer': q_name
+            });
+        }
+        else if (current_mode === 'ntf') {
+            obj.push({
+                'question': q_name,
+                'answer': q_formula
+            });
+        }
     });
     
     res.json(obj);
